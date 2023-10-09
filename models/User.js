f15ec0877556fa63d741eb1312d8eded4b99c6b6
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-10-09 20:54:54 +0300

    bcrypt install and password hashing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -25,6 +26,18 @@ const UserSchema = new mongoose.Schema({
 
 });
 
+UserSchema.pre("save", async function(){
+    if(!this.isModified("password")) {
+        next();
+    }
+
+    const  salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+
+})
+
 const User = mongoose.model("User", UserSchema);
+
 
 module.exports = User;
